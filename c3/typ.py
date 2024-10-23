8048a55d8e40f787026738f136856abfae9b54b9
Jakub Chmielak <jakub.chmielak@pw.edu.pl>
2024-10-23 10:19:16 +0200

    add

--- c3/typ.py
+++ c3/typ.py
@@ -1,3 +1,17 @@
 import constants
 
 print(f"Zmienna {constants.GRAVITY}")
+
+
+from constants import *  
+
+
+print(f"Pole koła to {PI} * ")
+print(f"Stała: {GRAVITY}")
+
+
+class Constants:
+    PI = 3.1415
+    GRAVITY = 9.81
+
+print(Constants.GRAVITY)
--- c3/typy_zmienne.py
+++ c3/typy_zmienne.py
@@ -24,18 +24,7 @@ print(wspolrzedne[0])
 stale = ("Kuba")
 print(stale)
 
-from constants import *  
 
-
-print(f"Pole koła to {PI} * ")
-print(f"Stała: {GRAVITY}")
-
-
-class Constants:
-    PI = 3.1415
-    GRAVITY = 9.81
-
-print(Constants.GRAVITY)
 
 samochod  = { "klucz" : "wartosc", "marka": "Topyota", "model":"Corolla", "rok": 2007}
 samochod['kolor'] = "czerwony"
